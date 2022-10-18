17b0710ec3d849ba1e8f24cab4703ce4c9185830
kumardeepak43 <105920038+kumardeepak43@users.noreply.github.com>
2022-10-18 13:41:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello, I am Deepak creating Node Js website ");
+const http=require("http");
+const data={
+  Name:"Deepak",
+  Age:"25",
+  Email:"17btcse063@shiats.edu.in",
+  City:"Allhabad",
+  State:"Uttar pradesh",
+}
+const jsonData=JSON.stringify(data);
+
+const server=http.createServer((request,response)=> {
+                              response.write=> (`<h1>Hello! Johan I am fullstack Devloper</h1>`);
+  response.write(jsonData);
+  response.end();
+})
+
+server.listen(9000,()=>{
+  console.log(
+    "Connected to port 9000");
+})
